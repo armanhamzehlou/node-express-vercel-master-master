5328a51af5700d83c6c5f2d36569209536c6a4fc
Arman Hamzehlou <arman.hamzehlou@ryerson.ca>
2022-11-24 15:04:23 -0500

    some new changes

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.send('respond with a resource');
 });
 
