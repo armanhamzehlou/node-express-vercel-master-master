d7eb3dfdd51f573768a793a51ae4d02c7403c366
Arman Hamzehlou <arman.hamzehlou@ryerson.ca>
2022-11-24 15:16:27 -0500

    Reduced

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 var express = require('express');
-var path = require('path');
-
-var indexRouter = require(__dirname + '/routes/index');
-var usersRouter = require(__dirname + '/routes/users');
-
 var app = express();
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get("/h1",(req,res)=>{
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end("hello")
 });
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+
 
 module.exports = app;
