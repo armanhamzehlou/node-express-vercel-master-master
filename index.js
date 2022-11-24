ff6557c9849407f5f9f731367ca24210e17518d4
Arman Hamzehlou <arman.hamzehlou@ryerson.ca>
2022-11-24 15:24:32 -0500

    minor rename

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + 'public'));
-
 
 app.get("/h1",(req,res)=>{
   res.setHeader('Content-Type', 'text/html');
